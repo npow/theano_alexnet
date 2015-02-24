74c9d77fc44c68c27beb3cc904f34d01d1854f2d
npow <nissan.pow@gmail.com>
2015-02-24 00:57:58 -0500

    .

--- alex_net.py
+++ alex_net.py
@@ -32,9 +32,9 @@ class AlexNet(object):
         weight_types = []
 
         if flag_datalayer:
-            data_layer = DataLayer(input=x, image_shape=(3, 256, 256,
+            data_layer = DataLayer(input=x, image_shape=(3, 48, 48,
                                                          batch_size),
-                                   cropsize=227, rand=rand, mirror=True,
+                                   cropsize=48, rand=rand, mirror=True,
                                    flag_rand=config['rand_crop'])
 
             layer1_input = data_layer.output
@@ -42,8 +42,8 @@ class AlexNet(object):
             layer1_input = x
 
         convpool_layer1 = ConvPoolLayer(input=layer1_input,
-                                        image_shape=(3, 227, 227, batch_size), 
-                                        filter_shape=(3, 11, 11, 96), 
+                                        image_shape=(3, 48, 48, batch_size), 
+                                        filter_shape=(3, 5, 5, 100), 
                                         convstride=4, padsize=0, group=1, 
                                         poolsize=3, poolstride=2, 
                                         bias_init=0.0, lrn=True,
@@ -54,8 +54,8 @@ class AlexNet(object):
         weight_types += convpool_layer1.weight_type
 
         convpool_layer2 = ConvPoolLayer(input=convpool_layer1.output,
-                                        image_shape=(96, 27, 27, batch_size),
-                                        filter_shape=(96, 5, 5, 256), 
+                                        image_shape=(256, 12, 12, batch_size),
+                                        filter_shape=(256, 3, 3, 200), 
                                         convstride=1, padsize=2, group=2, 
                                         poolsize=3, poolstride=2, 
                                         bias_init=0.1, lrn=True,
@@ -66,8 +66,8 @@ class AlexNet(object):
         weight_types += convpool_layer2.weight_type
 
         convpool_layer3 = ConvPoolLayer(input=convpool_layer2.output,
-                                        image_shape=(256, 13, 13, batch_size),
-                                        filter_shape=(256, 3, 3, 384), 
+                                        image_shape=(256, 12, 12, batch_size),
+                                        filter_shape=(256, 3, 3, 400), 
                                         convstride=1, padsize=1, group=1, 
                                         poolsize=1, poolstride=0, 
                                         bias_init=0.0, lrn=False,
@@ -78,8 +78,8 @@ class AlexNet(object):
         weight_types += convpool_layer3.weight_type
 
         convpool_layer4 = ConvPoolLayer(input=convpool_layer3.output,
-                                        image_shape=(384, 13, 13, batch_size),
-                                        filter_shape=(384, 3, 3, 384), 
+                                        image_shape=(256, 12, 12, batch_size),
+                                        filter_shape=(256, 3, 3, 400), 
                                         convstride=1, padsize=1, group=2, 
                                         poolsize=1, poolstride=0, 
                                         bias_init=0.1, lrn=False,
@@ -90,8 +90,8 @@ class AlexNet(object):
         weight_types += convpool_layer4.weight_type
 
         convpool_layer5 = ConvPoolLayer(input=convpool_layer4.output,
-                                        image_shape=(384, 13, 13, batch_size),
-                                        filter_shape=(384, 3, 3, 256), 
+                                        image_shape=(256, 12, 12, batch_size),
+                                        filter_shape=(256, 3, 3, 400), 
                                         convstride=1, padsize=1, group=2, 
                                         poolsize=3, poolstride=2, 
                                         bias_init=0.0, lrn=False,
@@ -103,22 +103,22 @@ class AlexNet(object):
 
         fc_layer6_input = T.flatten(
             convpool_layer5.output.dimshuffle(3, 0, 1, 2), 2)
-        fc_layer6 = FCLayer(input=fc_layer6_input, n_in=9216, n_out=4096)
+        fc_layer6 = FCLayer(input=fc_layer6_input, n_in=6400, n_out=512)
         self.layers.append(fc_layer6)
         params += fc_layer6.params
         weight_types += fc_layer6.weight_type
 
-        dropout_layer6 = DropoutLayer(fc_layer6.output, n_in=4096, n_out=4096)
+        dropout_layer6 = DropoutLayer(fc_layer6.output, n_in=512, n_out=512)
 
-        fc_layer7 = FCLayer(input=dropout_layer6.output, n_in=4096, n_out=4096)
+        fc_layer7 = FCLayer(input=dropout_layer6.output, n_in=512, n_out=512)
         self.layers.append(fc_layer7)
         params += fc_layer7.params
         weight_types += fc_layer7.weight_type
 
-        dropout_layer7 = DropoutLayer(fc_layer7.output, n_in=4096, n_out=4096)
+        dropout_layer7 = DropoutLayer(fc_layer7.output, n_in=512, n_out=512)
 
         softmax_layer8 = SoftmaxLayer(
-            input=dropout_layer7.output, n_in=4096, n_out=1000)
+            input=dropout_layer7.output, n_in=512, n_out=121)
         self.layers.append(softmax_layer8)
         params += softmax_layer8.params
         weight_types += softmax_layer8.weight_type
